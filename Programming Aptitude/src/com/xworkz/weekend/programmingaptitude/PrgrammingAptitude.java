4611237d2b56ed52f187f08e1e41a364538de5a3
anmol-garampalli <anmolgarampalli.xworkz@gmail.com>
2021-11-13 11:27:06 +0530

    All Programs

--- Programming Aptitude/src/com/xworkz/weekend/programmingaptitude/PrgrammingAptitude.java
+++ Programming Aptitude/src/com/xworkz/weekend/programmingaptitude/PrgrammingAptitude.java
@@ -5,13 +5,12 @@ public class PrgrammingAptitude {
 	public static void main(String[] args) {
 		for (int row = 1; row <= 5; row++) {
 
-			for (int col = 1; col <=row; col++) {
+			for (int col = 1; col <= row; col++) {
+				if (col <= row)
 				System.out.print("* ");
 			}
 			System.out.println();
 		}
-
-		
 
 	}
 
--- Programming Aptitude/src/com/xworkz/weekend/programmingaptitude/ReversStar.java
+++ Programming Aptitude/src/com/xworkz/weekend/programmingaptitude/ReversStar.java
@@ -3,10 +3,13 @@ package com.xworkz.weekend.programmingaptitude;
 public class ReversStar {
 
 	public static void main(String[] args) {
-		for (int row = 0; row <= 5; row++) {
-			for (int col = 0; col <= 5; col--) {
-				System.out.print("*");
-				
+		for (int row = 1; row >= 5; row++) {
+			for (int col = 1; col >= 5; col--) {
+				if (row <= col) {
+					System.out.println("*");
+
+				}
+
 			}
 			System.out.println();
 		}
