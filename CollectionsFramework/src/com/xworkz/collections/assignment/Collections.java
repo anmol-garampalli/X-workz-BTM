4dba74826af4332d9724ae4d9cc4d06eeea75500
anmolgarampalli2 <anmolgarampalli.xworkz@gmail.com>
2021-12-06 12:15:06 +0530

    Collections Assignment

---
+++ CollectionsFramework/src/com/xworkz/collections/assignment/Collections.java
@@ -0,0 +1,147 @@
+package com.xworkz.collections.assignment;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		String festival1="Republic Day";
+		String festival2="Dusherra";
+		String festival3="Makar Sankrantri";
+		String festival4="Pongal";
+		String festival5="Basant Panchami";
+		String festival6="Kumbh Mela";
+		String festival7=" Losar- Tibetan New Year";
+		String festival8="Maha Shivratri";
+		String festival9="Carnival in Goa";
+		String festival10="Wildlife";
+		String festival11="Holi";
+		String festival12="Easter";
+		String festival13=" Mahavir Jayanti";
+		String festival14=" Buddha Jayanti";
+		String festival15="Mango Season";
+		String festival16="Rath Yatra Orissa ";
+		String festival17="Eid ul-Fitr";
+		String festival18="Meditation Retreat";
+		String festival19="Rakshabandhan";
+		String festival20="Janmashtami";
+		String festival21="Independence Day";
+		String festival22="Nowruz-Parsi New Year";
+		String festival23="Muharram";
+		String festival24="Ganesh Chaturthi - Maharashtra";
+		String festival25="Onam ";
+		String festival26="Gandhi Jayanti";
+		String festival27=" Durga Puja";
+		String festival28="Navratri";
+		String festival29="Dussehra";
+		String festival30=" Diwali";
+		String festival31="Gurupurab or Guru Nanak Jayanti";
+		String festival32="Dev Deepawali";
+		String festival33="Beach Time";
+		String festival34="Marriage Season";
+		String festival35="New Year";
+		String festival36="Camel Treks";
+		String festival37=" Rann Utsav- Kutch Festival";
+		String festival38="Hornbill Festival, Nagaland";
+		String festival39="Akshaya Tritiya";
+		String festival40="Army Day";
+		String festival41="Auda pooja";
+		String festival42="Ahoi Ashtami";
+		String festival43="Ambedkar Jayanti"				;
+		String festival44="Anant Chaturdashi";
+		String festival45="Arbaeen";
+		String festival46="Buddha Purnima";
+		String festival47="Bhau-beej (Bhai Dooj) bathukamma";
+		String festival48="Carnival";
+		String festival49="Children's Day";
+		String festival50="Christmas Day";
+		String festival51="Cheti Chand";
+		String festival52="Chhath Puja";
+		String festival53="Engineer's Day";
+		String festival54="Eid al-Adha";
+		String festival55="Elephant Festival";
+		String festival56="Good Friday";
+		String festival57="Manusmriti Dahan Din";
+		String festival58="Pusnâ";
+		String festival59="Raksha Bandhan";
+		String festival60="Ram Navami";
+		
+		
+		Collection collection = new ArrayList(); 
+		
+		collection.add(festival1);
+		collection.add(festival2);
+		collection.add(festival3);
+		collection.add(festival4);
+		collection.add(festival5);
+		collection.add(festival6);
+		collection.add(festival7);
+		collection.add(festival8);
+		collection.add(festival9);
+		collection.add(festival10);
+		collection.add(festival11);
+		collection.add(festival12);
+		collection.add(festival13);
+		collection.add(festival14);
+		collection.add(festival15);
+		collection.add(festival16);
+		collection.add(festival17);
+		collection.add(festival18);
+		collection.add(festival19);
+		collection.add(festival20);
+		collection.add(festival21);
+		collection.add(festival22);
+		collection.add(festival23);
+		collection.add(festival24);
+		collection.add(festival25);
+		collection.add(festival26);
+		collection.add(festival27);
+		collection.add(festival28);
+		collection.add(festival29);
+		collection.add(festival30);
+		collection.add(festival31);
+		collection.add(festival32);
+		collection.add(festival33);
+		collection.add(festival34);
+		collection.add(festival35);
+		collection.add(festival36);
+		collection.add(festival37);
+		collection.add(festival38);
+		collection.add(festival39);
+		collection.add(festival40);
+		collection.add(festival41);
+		collection.add(festival42);
+		collection.add(festival43);
+		collection.add(festival44);
+		collection.add(festival45);
+		collection.add(festival46);
+		collection.add(festival47);
+		collection.add(festival48);
+		collection.add(festival49);
+		collection.add(festival50);
+		collection.add(festival51);
+		collection.add(festival52);
+		collection.add(festival53);
+		collection.add(festival54);
+		collection.add(festival55);
+		collection.add(festival56);
+		collection.add(festival57);
+		collection.add(festival58);
+		collection.add(festival59);
+		collection.add(festival60);
+		
+//		for (int i = 0; i < collection.size(); i++) {
+//			System.out.println(collection);
+//		}
+		
+		
+		
+		
+		
+		System.out.println(collection.size());
+		
+		
+	}
+
+}
